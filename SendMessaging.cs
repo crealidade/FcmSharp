f8b26b11c427e5a67ec240bd0e0243fed2f3dba1
Charles Martins <36443245+crealidade@users.noreply.github.com>
2021-06-01 13:29:43 -0300

    Add files via upload

---
+++ SendMessaging.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FcmSharp.Requests;
+using FcmSharp.Settings;
+
+namespace FcmSharp
+{
+    public class SendMessaging
+    {
+
+        public static string Execute(string title, string body, string topic, string key)
+        {
+
+            // Read the Credentials from a File, which is not under Version Control:
+            var settings = FileBasedFcmClientSettings.CreateFromFile(@key);
+
+            // Construct the Client:
+            using (var client = new FcmClient(settings))
+            {
+                var notification = new Notification
+                {
+                    Title = title,
+                    Body = body
+                };
+
+                // The Message should be sent to the News Topic:
+                var message = new FcmMessage()
+                {
+                    ValidateOnly = false,
+                    Message = new Message
+                    {
+                        Topic = topic,
+                        Notification = notification
+                    }
+                };
+
+                // Finally send the Message and wait for the Result:
+                CancellationTokenSource cts = new CancellationTokenSource();
+
+                // Send the Message and wait synchronously:
+                var result = client.SendAsync(message, cts.Token).GetAwaiter().GetResult();
+
+                // Print the Result to the Console:
+                return result.Name;
+
+            }
+        }
+    }
+
+}
